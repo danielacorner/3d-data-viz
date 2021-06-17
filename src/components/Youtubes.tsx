2f900a74802ce2dc09b6e898a0d44f7fa58e2415
Daniel Corner <danielcorner7@gmail.com>
2021-06-17 00:38:38 -0400

    add some youtubes around the center

--- src/components/Youtubes.tsx
+++ src/components/Youtubes.tsx
@@ -1,15 +1,13 @@
-import { Html } from "@react-three/drei";
+import { Billboard, Html } from "@react-three/drei";
 import ReactPlayer from "react-player";
 
 const PLAYER_DIMENSIONS = [2, 1, 0.1];
 
 /** a field of youtubes, walk around to get recommendations based on the current video */
 const Youtubes = () => {
-  const playerPositions: [number, number, number][] = [
-    [0, 0, 0],
-    [0, 0, 1],
-    [0, 0, 2],
-  ];
+  const playerPositions: [number, number, number][] = getDomeOfPositionsAround([
+    0, 0, 0,
+  ]);
   const players = playerPositions.map((position) => ({
     url: "https://www.youtube.com/watch?v=aK4JSwhdcdE",
     position,
@@ -35,42 +33,51 @@ function Player({
 }) {
   // const [isPlaying, setIsPlaying] = useState(false);
   return (
-    <mesh position={position}>
+    <Billboard position={position} {...({} as any)}>
       <boxBufferGeometry args={PLAYER_DIMENSIONS} />
       <meshBasicMaterial color="white" />
 
       {/* https://github.com/pmndrs/drei#html */}
       <Html className="react-player-wrapper" transform={true} sprite={false}>
         <ReactPlayer
+          width={530}
+          height={300}
+          style={{ transform: "scale(0.2)" }}
           // playing={isPlaying}
           url={url}
         />
       </Html>
-    </mesh>
+    </Billboard>
   );
 }
 
-// function useDisablePointerEventsWhileScrolling() {
-//   const ref = useRef(null as any);
-//   const timerRef = useRef(null as number | null);
-
-//   useEventListener("scroll", function () {
-//     if (!ref.current) {
-//       return;
-//     }
-
-//     if (timerRef.current) {
-//       window.clearTimeout(timerRef.current);
-//     }
-
-//     if (!ref.current.classList.contains(DISABLE_HOVER_CLASS)) {
-//       ref.current.classList.add(DISABLE_HOVER_CLASS);
-//     }
-
-//     timerRef.current = window.setTimeout(function () {
-//       ref.current.classList.remove(DISABLE_HOVER_CLASS);
-//     }, 500);
-//   });
-
-//   return ref;
-// }
+// dome = cube with 9 on each side (9+9+8=26 total)
+const SIDE_DISTANCE = 10;
+const sidesZ = [
+  SIDE_DISTANCE, // back
+  0, // center
+  -SIDE_DISTANCE, // front
+];
+const RELATIVE_DOME_POSITIONS = sidesZ.reduce(
+  (acc, s) => [
+    ...acc,
+    [0, 0, s], // center
+    [SIDE_DISTANCE, 0, s], // right
+    [-SIDE_DISTANCE, 0, s], // left
+    [0, SIDE_DISTANCE, s], // top
+    [0, -SIDE_DISTANCE, s], // bottom
+    // [d, d, s], // top right
+    // [-d, d, s], // top left
+    // [d, -d, s], // bottom right
+    // [-d, -d, s], // bottom left
+  ],
+  [] as number[][]
+);
+function getDomeOfPositionsAround(position: [number, number, number]) {
+  const [px, py, pz] = position;
+  return RELATIVE_DOME_POSITIONS.map(([dx, dy, dz]) => [
+    px + dx,
+    py + dy,
+    pz + dz,
+  ]) as [number, number, number][];
+}
