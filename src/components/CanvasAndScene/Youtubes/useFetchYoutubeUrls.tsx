971ded3d31a43fd87e11a316ce051d005fcd5a31
Daniel Corner <danielcorner7@gmail.com>
2021-06-19 04:01:25 -0400

    map response to youtube urls

--- src/components/CanvasAndScene/Youtubes/useFetchYoutubeUrls.tsx
+++ src/components/CanvasAndScene/Youtubes/useFetchYoutubeUrls.tsx
@@ -1,17 +1,13 @@
 import { useQuery } from "react-query";
-import { YOUTUBE_RESPONSE_EXAMPLE } from "./youtubeResponseExample";
-
-type YoutubeApiResponse = typeof YOUTUBE_RESPONSE_EXAMPLE; // TODO
+import { YoutubeResponseType } from "./youtubeResponseExample";
 
 export function useFetchYoutubeUrls({ enabled }) {
   const relatedToVideoId = "Ks-_Mh1QhMc";
 
   const apiKey = process.env.REACT_APP_YOUTUBE_API_KEY;
-  console.log("🌟🚨 ~ useFetchYoutubeUrls ~ process.env", process.env);
-  console.log("🌟🚨 ~ useFetchYoutubeUrls ~ apiKey", apiKey);
 
   // https://react-query.tanstack.com/reference/useQuery
-  const { isLoading, error, data } = useQuery<YoutubeApiResponse, Error>(
+  const { isLoading, error, data } = useQuery<YoutubeResponseType, Error>(
     `youtubeUrls-relatedToVideoId-${relatedToVideoId}`,
     () =>
       // relatedToVideoId
@@ -22,5 +18,12 @@ export function useFetchYoutubeUrls({ enabled }) {
     { refetchOnWindowFocus: false, enabled }
   );
 
-  return { isLoading, error, data };
+  return {
+    isLoading,
+    error,
+    data,
+    youtubeUrls: data?.items.map(
+      (item) => `https://www.youtube.com/watch?v=${item.id}`
+    ),
+  };
 }
--- src/components/CanvasAndScene/Youtubes/youtubeResponseExample.tsx
+++ src/components/CanvasAndScene/Youtubes/youtubeResponseExample.tsx
@@ -1,144 +1,16 @@
+export type YoutubeResponseType = typeof YOUTUBE_RESPONSE_EXAMPLE;
+export type YoutubeItemType = typeof YOUTUBE_ITEM_RESPONSE_EXAMPLE;
+
 export const YOUTUBE_RESPONSE_EXAMPLE = {
   kind: "youtube#searchListResponse",
-  etag: "IxXcd93pLfAybfc4t7xdKNWSIjk",
+  etag: "83O37KPIjMv0FmUWsZ-s7B0CSlA",
   nextPageToken: "CAUQAA",
   regionCode: "CA",
   pageInfo: {
-    totalResults: 199,
+    totalResults: 198,
     resultsPerPage: 5,
   },
   items: [
-    {
-      kind: "youtube#searchResult",
-      etag: "1YA7h8DQ3ridfRfCQ_MDMZH6P-U",
-      id: {
-        kind: "youtube#video",
-        videoId: "HZt5CVSzvH8",
-      },
-      snippet: {
-        publishedAt: "2016-08-02T14:45:13Z",
-        channelId: "UCxANyRzvWuk9A5r71joGNNg",
-        title: "The Future of StoryTelling Festival trailer",
-        description:
-          "http://www.fost.org/fest\n\nJoin us in New York City for the world’s first immersive storytelling festival!\nFoST FEST will feature more than 75,000 square feet of exhibition space, hands-on technology demos, interactive installations, and a multiperson VR theater, plus immersive performances, live-action role-playing games, flash mobs, and other events in partnership with arts organizations, theater troupes, and experience producers around New York City.\n\n\nThis trailer is 360-enabled - make sure to watch on a desktop or in your YouTube mobile app for best experience!\n\nThe film includes clips from Help! (Google Spotlight Stories), Pearl (Google Spotlight Stories), I, Philip (Pierre Zandrowicz), A History of Cuban Dance (Here Be Dragons), Waves (Wevr), Stor Eiglass (Marshmallow Laser Feast), and Introduction to Virtual Reality (Felix & Paul Studios)",
-        thumbnails: {
-          default: {
-            url: "https://i.ytimg.com/vi/HZt5CVSzvH8/default.jpg",
-            width: 120,
-            height: 90,
-          },
-          medium: {
-            url: "https://i.ytimg.com/vi/HZt5CVSzvH8/mqdefault.jpg",
-            width: 320,
-            height: 180,
-          },
-          high: {
-            url: "https://i.ytimg.com/vi/HZt5CVSzvH8/hqdefault.jpg",
-            width: 480,
-            height: 360,
-          },
-          standard: {
-            url: "https://i.ytimg.com/vi/HZt5CVSzvH8/sddefault.jpg",
-            width: 640,
-            height: 480,
-          },
-          maxres: {
-            url: "https://i.ytimg.com/vi/HZt5CVSzvH8/maxresdefault.jpg",
-            width: 1280,
-            height: 720,
-          },
-        },
-        channelTitle: "Future of StoryTelling",
-        liveBroadcastContent: "none",
-        publishTime: "2016-08-02T14:45:13Z",
-      },
-    },
-    {
-      kind: "youtube#searchResult",
-      etag: "ucGtXq_Pm86Vn0XLTaBNVYjPwJA",
-      id: {
-        kind: "youtube#video",
-        videoId: "vEo19grQLRw",
-      },
-      snippet: {
-        publishedAt: "2020-02-20T00:09:05Z",
-        channelId: "UCfYpELrp-BocmZAvru4r3gg",
-        title: "VR180 Elevator Pitch",
-        description:
-          "Fred Kolman VR180 Pitch\r\nAbout VR180 \r\nadditional Text 2",
-        thumbnails: {
-          default: {
-            url: "https://i.ytimg.com/vi/vEo19grQLRw/default.jpg",
-            width: 120,
-            height: 90,
-          },
-          medium: {
-            url: "https://i.ytimg.com/vi/vEo19grQLRw/mqdefault.jpg",
-            width: 320,
-            height: 180,
-          },
-          high: {
-            url: "https://i.ytimg.com/vi/vEo19grQLRw/hqdefault.jpg",
-            width: 480,
-            height: 360,
-          },
-          standard: {
-            url: "https://i.ytimg.com/vi/vEo19grQLRw/sddefault.jpg",
-            width: 640,
-            height: 480,
-          },
-        },
-        channelTitle: "Fred Kolman",
-        liveBroadcastContent: "none",
-        publishTime: "2020-02-20T00:09:05Z",
-      },
-    },
-    {
-      kind: "youtube#searchResult",
-      etag: "dUmJduVCh4IYFZAB__84kwiqmDo",
-      id: {
-        kind: "youtube#video",
-        videoId: "ATo9sYax-AQ",
-      },
-      snippet: {
-        publishedAt: "2019-01-31T16:19:51Z",
-        channelId: "UCuIFW5miIr0y_-SKk3ex5yQ",
-        title:
-          "Presence: Bringing Your Boldest Self to Your Biggest Challenges | Dr. Amy Cuddy | IDEAcademy 2018",
-        description:
-          "Some of life’s biggest hurdles call for moments of sincerity and control. Too often, we approach these high-pressure moments with fear and execute with anxiety. Based on her best-selling book Presence: Bringing Your Boldest Self to Your Biggest Challenges, Harvard Business School professor and social psychologist Amy Cuddy shares revolutionary research and personal narratives in this impassioned, engaging, and innovative presentation. You will learn simple techniques to complete inner transformation, harness the power of presence, and perform at the highest levels of confidence.",
-        thumbnails: {
-          default: {
-            url: "https://i.ytimg.com/vi/ATo9sYax-AQ/default.jpg",
-            width: 120,
-            height: 90,
-          },
-          medium: {
-            url: "https://i.ytimg.com/vi/ATo9sYax-AQ/mqdefault.jpg",
-            width: 320,
-            height: 180,
-          },
-          high: {
-            url: "https://i.ytimg.com/vi/ATo9sYax-AQ/hqdefault.jpg",
-            width: 480,
-            height: 360,
-          },
-          standard: {
-            url: "https://i.ytimg.com/vi/ATo9sYax-AQ/sddefault.jpg",
-            width: 640,
-            height: 480,
-          },
-          maxres: {
-            url: "https://i.ytimg.com/vi/ATo9sYax-AQ/maxresdefault.jpg",
-            width: 1280,
-            height: 720,
-          },
-        },
-        channelTitle: "Eastman",
-        liveBroadcastContent: "none",
-        publishTime: "2019-01-31T16:19:51Z",
-      },
-    },
     {
       kind: "youtube#searchResult",
       etag: "R-Ph-3xaL3p8yPsFeFmayfXrwcI",
@@ -224,5 +96,145 @@ export const YOUTUBE_RESPONSE_EXAMPLE = {
         publishTime: "2011-10-13T19:33:04Z",
       },
     },
-  ],
+    {
+      kind: "youtube#searchResult",
+      etag: "dUmJduVCh4IYFZAB__84kwiqmDo",
+      id: {
+        kind: "youtube#video",
+        videoId: "ATo9sYax-AQ",
+      },
+      snippet: {
+        publishedAt: "2019-01-31T16:19:51Z",
+        channelId: "UCuIFW5miIr0y_-SKk3ex5yQ",
+        title:
+          "Presence: Bringing Your Boldest Self to Your Biggest Challenges | Dr. Amy Cuddy | IDEAcademy 2018",
+        description:
+          "Some of life’s biggest hurdles call for moments of sincerity and control. Too often, we approach these high-pressure moments with fear and execute with anxiety. Based on her best-selling book Presence: Bringing Your Boldest Self to Your Biggest Challenges, Harvard Business School professor and social psychologist Amy Cuddy shares revolutionary research and personal narratives in this impassioned, engaging, and innovative presentation. You will learn simple techniques to complete inner transformation, harness the power of presence, and perform at the highest levels of confidence.",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/ATo9sYax-AQ/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/ATo9sYax-AQ/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/ATo9sYax-AQ/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+          standard: {
+            url: "https://i.ytimg.com/vi/ATo9sYax-AQ/sddefault.jpg",
+            width: 640,
+            height: 480,
+          },
+          maxres: {
+            url: "https://i.ytimg.com/vi/ATo9sYax-AQ/maxresdefault.jpg",
+            width: 1280,
+            height: 720,
+          },
+        },
+        channelTitle: "Eastman",
+        liveBroadcastContent: "none",
+        publishTime: "2019-01-31T16:19:51Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "YTdunWSnvmzv50Y7uiD6FreuZkY",
+      id: {
+        kind: "youtube#video",
+        videoId: "c0KYU2j0TM4",
+      },
+      snippet: {
+        publishedAt: "2012-03-02T18:32:02Z",
+        channelId: "UCAuUUnT6oDeKwE6v1NGQxug",
+        title: "The power of introverts | Susan Cain",
+        description:
+          "Visit http://TED.com to get our entire library of TED Talks, transcripts, translations, personalized talk recommendations and more.\n\nIn a culture where being social and outgoing are prized above all else, it can be difficult, even shameful, to be an introvert. But, as Susan Cain argues in this passionate talk, introverts bring extraordinary talents and abilities to the world, and should be encouraged and celebrated.\n\nThe TED Talks channel features the best talks and performances from the TED Conference, where the world's leading thinkers and doers give the talk of their lives in 18 minutes (or less). Look for talks on Technology, Entertainment and Design -- plus science, business, global issues, the arts and more. You're welcome to link to or embed these videos, forward them to others and share these ideas with people you know. \n\nFollow TED on Twitter: http://twitter.com/TEDTalks\nLike TED on Facebook: http://facebook.com/TED\nSubscribe to our channel: http://youtube.com/TED\n\nTED's videos may be used for non-commercial purposes under a Creative Commons License, Attribution–Non Commercial–No Derivatives (or the CC BY – NC – ND 4.0 International) and in accordance with our TED Talks Usage Policy (https://www.ted.com/about/our-organization/our-policies-terms/ted-talks-usage-policy). For more information on using TED for commercial purposes (e.g. employee learning, in a film or online course), please submit a Media Request at https://media-requests.ted.com",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/c0KYU2j0TM4/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/c0KYU2j0TM4/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/c0KYU2j0TM4/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+          standard: {
+            url: "https://i.ytimg.com/vi/c0KYU2j0TM4/sddefault.jpg",
+            width: 640,
+            height: 480,
+          },
+          maxres: {
+            url: "https://i.ytimg.com/vi/c0KYU2j0TM4/maxresdefault.jpg",
+            width: 1280,
+            height: 720,
+          },
+        },
+        channelTitle: "TED",
+        liveBroadcastContent: "none",
+        publishTime: "2012-03-02T18:32:02Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "_QkBFRw8gmc1xTBwG9Z5bfg3j2Q",
+      id: {
+        kind: "youtube#video",
+        videoId: "AdeF9baZg1g",
+      },
+    },
+  ] as YoutubeItemType[],
+};
+
+export const YOUTUBE_ITEM_RESPONSE_EXAMPLE = {
+  kind: "youtube#searchResult",
+  etag: "p9EahI_H8EgTN_GsNeNeB8vw8I0",
+  id: {
+    kind: "youtube#video",
+    videoId: "P_6vDLq64gE",
+  },
+  snippet: {
+    publishedAt: "2011-10-13T19:33:04Z",
+    channelId: "UCAuUUnT6oDeKwE6v1NGQxug",
+    title: "How to spot a liar | Pamela Meyer",
+    description:
+      "Visit http://TED.com to get our entire library of TED Talks, transcripts, translations, personalized talk recommendations and more.\n\nOn any given day we're lied to from 10 to 200 times, and the clues to detect those lies can be subtle and counter-intuitive. Pamela Meyer, author of \"Liespotting,\" shows the manners and \"hotspots\" used by those trained to recognize deception -- and she argues honesty is a value worth preserving. (Contains mature content)\n\nThe TED Talks channel features the best talks and performances from the TED Conference, where the world's leading thinkers and doers give the talk of their lives in 18 minutes (or less). Look for talks on Technology, Entertainment and Design -- plus science, business, global issues, the arts and more. You're welcome to link to or embed these videos, forward them to others and share these ideas with people you know. \n\nFollow TED on Twitter: http://twitter.com/TEDTalks\nLike TED on Facebook: http://facebook.com/TED\nSubscribe to our channel: http://youtube.com/TED\n\nTED's videos may be used for non-commercial purposes under a Creative Commons License, Attribution–Non Commercial–No Derivatives (or the CC BY – NC – ND 4.0 International) and in accordance with our TED Talks Usage Policy (https://www.ted.com/about/our-organization/our-policies-terms/ted-talks-usage-policy). For more information on using TED for commercial purposes (e.g. employee learning, in a film or online course), please submit a Media Request at https://media-requests.ted.com",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/P_6vDLq64gE/default.jpg",
+        width: 120,
+        height: 90,
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/P_6vDLq64gE/mqdefault.jpg",
+        width: 320,
+        height: 180,
+      },
+      high: {
+        url: "https://i.ytimg.com/vi/P_6vDLq64gE/hqdefault.jpg",
+        width: 480,
+        height: 360,
+      },
+      standard: {
+        url: "https://i.ytimg.com/vi/P_6vDLq64gE/sddefault.jpg",
+        width: 640,
+        height: 480,
+      },
+    },
+    channelTitle: "TED",
+    liveBroadcastContent: "none",
+    publishTime: "2011-10-13T19:33:04Z",
+  },
 };
