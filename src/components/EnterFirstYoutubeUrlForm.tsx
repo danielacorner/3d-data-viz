bd3f0c4002ff83d28423a68145af8a3e3cd5f0cf
Daniel Corner <danielcorner7@gmail.com>
2021-06-20 02:54:14 -0400

    form styles

--- src/components/EnterFirstYoutubeUrlForm.tsx
+++ src/components/EnterFirstYoutubeUrlForm.tsx
@@ -3,14 +3,17 @@ import { useState } from "react";
 import { useAtom } from "jotai";
 import { initialYoutubeIdAtom } from "../store/store";
 import styled from "styled-components/macro";
+import ReactPlayer from "react-player";
 
 export function EnterFirstYoutubeUrlForm() {
   const [open, setOpen] = useState(true);
   const [value, setValue] = useState("youtube.com/watch?v=eCbyqm9jcBA");
   const [, setInitialYoutubeId] = useAtom(initialYoutubeIdAtom);
+
   return (
     <Dialog open={open}>
       <EnterFirstYoutubeUrlFormStyles>
+        <h1 className="title">🌳 youtube-forest</h1>
         <form
           onSubmit={(e) => {
             e.preventDefault();
@@ -20,28 +23,68 @@ export function EnterFirstYoutubeUrlForm() {
           }}
         >
           <TextField
-            label="Enter a Youtube URL or video ID"
+            label="Enter a Youtube URL or ID"
             value={value}
             onChange={(e) => setValue(e.target.value)}
+            onFocus={(event) => event.target.select()}
           />
           <Button type="submit" disabled={!value}>
             Go!
           </Button>
         </form>
+        {getIsValueValid(value) && (
+          <div className="player">
+            <ReactPlayer
+              // onClickPreview={handleClickPreview}
+              width={260}
+              height={150}
+              // playing={isPlaying}
+              light={true}
+              url={value}
+            />
+          </div>
+        )}
       </EnterFirstYoutubeUrlFormStyles>
     </Dialog>
   );
 }
+function getIsValueValid(value: string) {
+  const isYoutubeDotCom =
+    value.includes("youtube.com") &&
+    value.length >= "youtube.com/watch?v=eCbyqm9jcBA".length;
+  const isYoutuDotBe =
+    value.includes("youtu.be") &&
+    value.length >= "youtu.be/watch?v=eCbyqm9jcBA".length;
+  return isYoutubeDotCom || isYoutuDotBe;
+}
+
 const EnterFirstYoutubeUrlFormStyles = styled.div`
+  max-width: calc(100vw - 32px);
+  padding: 0.5em 1em;
   form {
     display: flex;
     flex-direction: column;
     align-items: center;
     gap: 10px;
-    padding: 20px 40px;
   }
   .MuiTextField-root {
-    width: 300px;
+    min-width: 260px;
+  }
+  input {
+    font-size: 0.8em;
+    margin-top: 0.5em;
+  }
+  .title {
+    font-size: 1em;
+  }
+  position: relative;
+  .player {
+    background: white;
+    margin: auto;
+    position: absolute;
+    bottom: -300px;
+    left: 0;
+    right: 0;
   }
 `;
 function getIdFromValue(value: string): string {
